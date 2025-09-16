09db36bb38a31631666ebbf2ccfde4773b271994
WiseAdviceIM <wiseadviceim@roadmapschool.ru>
2025-09-16 17:39:04 +0300

    home-work-kotlin-2

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -1,6 +1,40 @@
 package ru.otus.homework
 
+fun main() {
+    val testResult = fizzbuzz(5)
+    println("Результат для n=5: ${testResult.contentToString()}")
 
+    val testResult2 = fizzbuzz(16)
+    println("Результат для n=16: ${testResult2.contentToString()}")
+}
 fun fizzbuzz(n: Int): Array<String> {
-    TODO("Выполните задание")
+
+    val resultWithWhen = Array(n) { "" }
+    for (i in 0 until n) {
+        resultWithWhen[i] = when {
+            i % 3 == 0 && i % 5 == 0 -> "FizzBuzz"
+            i % 3 == 0 -> "Fizz"
+            i % 5 == 0 -> "Buzz"
+            else -> i.toString()
+        }
+    }
+
+    val resultWithIf = Array(n) { "" }
+    for (i in 0 until n) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            resultWithIf[i] = "FizzBuzz"
+        } else if (i % 3 == 0) {
+            resultWithIf[i] = "Fizz"
+        } else if (i % 5 == 0) {
+            resultWithIf[i] = "Buzz"
+        } else {
+            resultWithIf[i] = i.toString()
+        }
+    }
+
+    if (!resultWithWhen.contentEquals(resultWithIf)) {
+        throw IllegalStateException("Результаты способов не совпадают!")
+    }
+
+    return resultWithWhen
 }
--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -1,6 +1,34 @@
 package ru.otus.homework
 
+fun main() {
+    val numbers1 = intArrayOf(2, 7, 11, 15)
+    val target1 = 9
+    val result1 = sumOfTwo(numbers1, target1)
+    println("Результат 1: ${result1.contentToString()}")
+
+    val numbers2 = intArrayOf(3, 2, 4)
+    val target2 = 6
+    val result2 = sumOfTwo(numbers2, target2)
+    println("Результат 2: ${result2.contentToString()}")
+
+    val numbers3 = intArrayOf(1, 2, 3)
+    val target3 = 10
+    try {
+        val result3 = sumOfTwo(numbers3, target3)
+        println("Результат 3: ${result3.contentToString()}")
+    } catch (e: IllegalArgumentException) {
+        println("Ошибка: ${e.message}")
+    }
+}
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    TODO("Выполните задание")
+    for (i in numbers.indices) {
+        for (j in i + 1 until numbers.size) {
+            if (numbers[i] + numbers[j] == target) {
+                return intArrayOf(i, j)
+            }
+        }
+    }
+
+    throw IllegalArgumentException("Пары чисел с заданной суммой не существует")
 }
